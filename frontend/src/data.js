332e14bf44d9126bfc4bc6d134e5e7833880a925
Jorge Sanchez <jorgesinsilla@gmail.com>
2023-05-21 17:18:29 +0100

    create-products

---
+++ frontend/src/data.js
@@ -0,0 +1,42 @@
+const data = {
+    products: [
+        {
+            name: 'Pokemon Gold Game Boy',
+            slug: 'pokemon-gold-gb',
+            category: 'Videogames',
+            image: '/images/pokemon-gold-gb.jpg',
+            price: 15,
+            countInStock: 3,
+            brand: 'Nintendo',
+            rating: 4.8,
+            numReviews: 12,
+            description: 'enjoy'
+        },
+        {
+            name: 'Game Boy Color',
+            slug: 'game-boy-color',
+            category: 'Videogames',
+            image: '/images/game-boy-color.jpg',
+            price: 60,
+            countInStock: 2,
+            brand: 'Nintendo',
+            rating: 4.9,
+            numReviews: 102,
+            description: 'Wanna be a child again?'
+        },
+        {
+            name: 'Game Boy Advance',
+            slug: 'game-boy-advance',
+            category: 'Videogames',
+            image: '/images/game-boy-advance.jpg',
+            price: 40,
+            countInStock: 4,
+            brand: 'Nintendo',
+            rating: 4.2,
+            numReviews: 54,
+            description: 'Never stop playing!'
+        }
+    ]
+}
+
+export default data;
