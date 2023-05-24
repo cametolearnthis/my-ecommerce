9fac983799a2b41ed011ef0ab49ed43083ea2b5b
Jorge Sanchez <jorgesinsilla@gmail.com>
2023-05-24 20:58:36 +0100

    create users sample

--- backend/data.js
+++ backend/data.js
@@ -1,45 +1,62 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
-    products: [
-        {
-            //_id: '1',
-            name: 'Pokemon Gold Game Boy',
-            slug: 'pokemon-gold-gb',
-            category: 'Videogames',
-            image: '/images/pokemon-gold-gb.jpg',
-            price: 15,
-            countInStock: 0,
-            brand: 'Nintendo',
-            rating: 4.8,
-            numReviews: 12,
-            description: 'enjoy'
-        },
-        {
-            //_id: '2',
-            name: 'Game Boy Color',
-            slug: 'game-boy-color',
-            category: 'Videogames',
-            image: '/images/game-boy-color.jpg',
-            price: 60,
-            countInStock: 2,
-            brand: 'Nintendo',
-            rating: 4.9,
-            numReviews: 102,
-            description: 'Wanna be a child again?'
-        },
-        {
-           // _id: '3',
-            name: 'Game Boy Advance',
-            slug: 'game-boy-advance',
-            category: 'Videogames',
-            image: '/images/game-boy-advance.jpg',
-            price: 40,
-            countInStock: 4,
-            brand: 'Nintendo',
-            rating: 4.2,
-            numReviews: 54,
-            description: 'Never stop playing!'
-        }
-    ]
-}
+  users: [
+    {
+      name: 'Jorge',
+      email: 'jorge@jorge.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: true,
+    },
+    {
+      name: 'John',
+      email: 'user@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: false,
+    },
+  ],
+
+  products: [
+    {
+      //_id: '1',
+      name: 'Pokemon Gold Game Boy',
+      slug: 'pokemon-gold-gb',
+      category: 'Videogames',
+      image: '/images/pokemon-gold-gb.jpg',
+      price: 15,
+      countInStock: 0,
+      brand: 'Nintendo',
+      rating: 4.8,
+      numReviews: 12,
+      description: 'enjoy',
+    },
+    {
+      //_id: '2',
+      name: 'Game Boy Color',
+      slug: 'game-boy-color',
+      category: 'Videogames',
+      image: '/images/game-boy-color.jpg',
+      price: 60,
+      countInStock: 2,
+      brand: 'Nintendo',
+      rating: 4.9,
+      numReviews: 102,
+      description: 'Wanna be a child again?',
+    },
+    {
+      // _id: '3',
+      name: 'Game Boy Advance',
+      slug: 'game-boy-advance',
+      category: 'Videogames',
+      image: '/images/game-boy-advance.jpg',
+      price: 40,
+      countInStock: 4,
+      brand: 'Nintendo',
+      rating: 4.2,
+      numReviews: 54,
+      description: 'Never stop playing!',
+    },
+  ],
+};
 
 export default data;
