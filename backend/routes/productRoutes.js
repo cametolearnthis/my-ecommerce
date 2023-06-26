7521a390b9095e1e7f4cd018f22dd07bb85e209e
Jorge Sanchez <jorgesinsilla@gmail.com>
2023-06-26 17:32:52 +0100

    create sidebar & search box

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import Product from '../models/productModel.js';
+import expressAsyncHandler from 'express-async-handler';
 
 const productRouter = express.Router();
 
@@ -8,15 +9,23 @@ productRouter.get('/', async (req, res) => {
   res.send(products);
 });
 
-productRouter.get("/slug/:slug", async (req, res) => {
-    const product = await Product.findOne({ slug: { $eq: req.params.slug } });
-  
-    if (product) {
-      res.send(product);
-    } else {
-      res.status(404).send({ message: "Product not found" });
-    }
-  });
+productRouter.get(
+  '/categories',
+  expressAsyncHandler(async (req, res) => {
+    const categories = await Product.find().distinct('category');
+    res.send(categories);
+  })
+);
+
+productRouter.get('/slug/:slug', async (req, res) => {
+  const product = await Product.findOne({ slug: { $eq: req.params.slug } });
+
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: 'Product not found' });
+  }
+});
 
 productRouter.get('/:id', async (req, res) => {
   const product = await Product.findById(req.params.id);
